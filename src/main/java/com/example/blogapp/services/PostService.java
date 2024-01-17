cbcc789a96fc6b3bbae52a8f6729b9a87f9b1248
amt-isaac-agbobli <isaac.agbobli@amalitech.org>
2024-01-17 03:07:24 -1200

    feat: add service method for getting posts

--- src/main/java/com/example/blogapp/services/PostService.java
+++ src/main/java/com/example/blogapp/services/PostService.java
@@ -2,18 +2,23 @@ package com.example.blogapp.services;
 
 import com.example.blogapp.models.Post;
 import com.example.blogapp.utils.Category;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-
+@Service
 public class PostService {
-    private static  List<Post> posts = new ArrayList<>();
+    private static final List<Post> posts = new ArrayList<>();
 
      static {
          posts.add(new Post(1L, "First Post", "This is my first post", "John Doe",Category.SPORT));
-         posts.add(new Post(2L, "Second Post", "This is my second post", "John Doe",Category.POLITICS));
-         posts.add(new Post(3L, "Third Post", "This is my third post", "John Doe",Category.ECONOMY));
-         posts.add(new Post(4L, "Fourth Post", "This is my fourth post", "John Doe",Category.TECHNOLOGY));
+         posts.add(new Post(2L, "Second Post", "This is my second post", "Christ John",Category.POLITICS));
+         posts.add(new Post(3L, "Third Post", "This is my third post", "Ali Doe",Category.ECONOMY));
+         posts.add(new Post(4L, "Fourth Post", "This is my fourth post", "John Isaac",Category.TECHNOLOGY));
      }
+
+        public List<Post> getPosts() throws InterruptedException {
+            return posts;
+        }
 
 }
