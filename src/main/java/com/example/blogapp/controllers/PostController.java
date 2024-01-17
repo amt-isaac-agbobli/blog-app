1e1242280e02fe607e44c2f65921d254f37151e3
amt-isaac-agbobli <isaac.agbobli@amalitech.org>
2024-01-17 04:02:27 -1200

    feat: add a controller for getting single post by ID

--- src/main/java/com/example/blogapp/controllers/PostController.java
+++ src/main/java/com/example/blogapp/controllers/PostController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -23,5 +24,10 @@ public class PostController implements PostControllerInterface {
     public String getPosts(Model model) {
         model.addAttribute("posts", postService.getPosts());
         return "posts";
+    }
+    @GetMapping(path = "/{id}")
+    public String getPostById(@PathVariable() Long id, Model model) {
+        model.addAttribute("post", postService.getPostById(id));
+        return "post";
     }
 }
--- src/main/java/com/example/blogapp/interfaces/PostControllerInterface.java
+++ src/main/java/com/example/blogapp/interfaces/PostControllerInterface.java
@@ -4,4 +4,5 @@ import org.springframework.ui.Model;
 
 public interface PostControllerInterface {
     public String getPosts(Model model);
+    public String getPostById(Long id, Model model);
 }
