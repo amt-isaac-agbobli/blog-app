6548e56747bf05e96aa9293ad5397e1f262b581e
amt-isaac-agbobli <isaac.agbobli@amalitech.org>
2024-01-17 03:19:04 -1200

    feat: add controller for getting posts

---
+++ src/main/java/com/example/blogapp/controllers/PostController.java
@@ -0,0 +1,26 @@
+package com.example.blogapp.controllers;
+
+import com.example.blogapp.models.Post;
+import com.example.blogapp.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/posts")
+public class PostController {
+    private  final PostService postService;
+    @Autowired
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+    @GetMapping
+    public String getPosts(Model model) {
+        model.addAttribute("posts", postService.getPosts());
+        return "posts";
+    }
+}
