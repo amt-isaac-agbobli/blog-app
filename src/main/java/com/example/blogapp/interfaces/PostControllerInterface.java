8b86a04f9e2a48de705346569fd3840b752cd2ee
amt-isaac-agbobli <isaac.agbobli@amalitech.org>
2024-01-17 22:00:34 -1200

    feat: add complate the feature for adding post

--- src/main/java/com/example/blogapp/interfaces/PostControllerInterface.java
+++ src/main/java/com/example/blogapp/interfaces/PostControllerInterface.java
@@ -7,5 +7,5 @@ public interface PostControllerInterface {
     public String getPosts(Model model);
     public String getPostById(Long id, Model model);
     public String getCreatePostPage(Model model);
-//    public  String createPost(Post post, Model model);
+    public  String createPost(Post post);
 }
