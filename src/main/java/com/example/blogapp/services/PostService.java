c8e1d8740d4577305b59720bdf4b749b8fbbdf4e
amt-isaac-agbobli <isaac.agbobli@amalitech.org>
2024-01-18 21:10:48 -1200

    feat: swap the array with database

--- src/main/java/com/example/blogapp/services/PostService.java
+++ src/main/java/com/example/blogapp/services/PostService.java
@@ -2,7 +2,6 @@ package com.example.blogapp.services;
 
 import com.example.blogapp.interfaces.PostServiceInterface;
 import com.example.blogapp.models.Post;
-import com.example.blogapp.enums.Category;
 import com.example.blogapp.repositories.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,10 +13,10 @@ public class PostService implements PostServiceInterface {
     private static final List<Post> posts = new ArrayList<>();
 
      static {
-         posts.add(new Post(1L, "First Post", "This is my first post", "John Doe",Category.SPORT));
-         posts.add(new Post(2L, "Second Post", "This is my second post", "Christ John",Category.POLITICS));
-         posts.add(new Post(3L, "Third Post", "This is my third post", "Ali Doe",Category.ECONOMY));
-         posts.add(new Post(4L, "Fourth Post", "This is my fourth post", "John Isaac",Category.TECHNOLOGY));
+         posts.add(new Post(1L, "First Post", "This is my first post", "John Doe", "SPORT"));
+         posts.add(new Post(2L, "Second Post", "This is my second post", "Christ John", "POLITICS"));
+         posts.add(new Post(3L, "Third Post", "This is my third post", "Ali Doe", "ECONOMY"));
+         posts.add(new Post(4L, "Fourth Post", "This is my fourth post", "John Isaac", "TECHNOLOGY"));
      }
      private final PostRepository postRepository;
     @Autowired
@@ -31,26 +30,25 @@ public class PostService implements PostServiceInterface {
 
 
         public Post getPostById(Long id) {
-            return posts.stream()
-                    .filter(post -> post.getId() == id)
-                    .findFirst()
-                    .orElse(null);
+            return postRepository.findById(id).orElse(null);
         }
 
         public void createPost(Post post) {
-            posts.add(post);
+            postRepository.save(post);
         }
 
         public void deletePost(Long id) {
-            posts.removeIf(post -> post.getId() == id);
+            postRepository.deleteById(id);
         }
 
         public void updatePost(Long id, Post post) {
-            Post oldPost = getPostById(id);
-            oldPost.setTitle(post.getTitle());
-            oldPost.setAuthor(post.getAuthor());
-            oldPost.setContent(post.getContent());
-            oldPost.setCategory(post.getCategory());
+            Post postToUpdate = postRepository.findById(id).orElse(null);
+            assert postToUpdate != null;
+            postToUpdate.setTitle(post.getTitle());
+            postToUpdate.setContent(post.getContent());
+            postToUpdate.setAuthor(post.getAuthor());
+            postToUpdate.setCategory(post.getCategory());
+            postRepository.save(postToUpdate);
         }
 
 }
